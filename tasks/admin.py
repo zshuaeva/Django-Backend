5f4d90042867ca4a284a04bd949d2461a693dad9
Joshua <jpe7619@gmail.com>
2023-01-23 21:20:22 -0800

    Feature 12 complete

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from tasks.models import Task
 
-# Register your models here.
+
+@admin.register(Task)
+class Task(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "start_date",
+        "due_date",
+        "is_completed",
+        "project",
+        "assignee"
+    )
