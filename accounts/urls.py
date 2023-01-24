f5c33377ccd177b2a91c6b5c1ecea2702ce0705e
Joshua <jpe7619@gmail.com>
2023-01-23 20:31:28 -0800

    Feature 9 complete

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
-from accounts.views import user_login
+from accounts.views import user_login, user_logout
 from django.urls import path
 
 
 urlpatterns = [
     path("login/", user_login, name="login"),
+    path("logout/", user_logout, name="logout")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 from accounts.forms import LoginForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 
 def user_login(request):
@@ -24,3 +24,8 @@ def user_login(request):
         "form": form,
     }
     return render(request, "login.html", context)
+
+
+def user_logout(request):
+    logout(request)
+    return redirect("login")
